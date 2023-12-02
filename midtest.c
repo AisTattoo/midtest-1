6b1f190ea3617204114e1e2dd126ec20dfe3af16
AisTattoo <adilmubarak126@gmail.com>
2023-12-02 10:39:24 +0700

    uts alpro adil

---
+++ midtest.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    float panjang, lebar, luas;
+    printf("Masukkan panjang : ");
+    scanf("%f", &panjang);
+    printf("Masukkan lebar : ");
+    scanf("%f", &lebar);
+    luas = panjang * lebar;
+    printf("Luas persegi panjang adalah: %.2f\n",  luas);
+    return 0;
+}
